f4ebb988d802053a67910b4b4bb795b35ea59372
sediq <you@example.com>
2020-01-01 00:00:00 +0100

    test

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use("/assets", express.static(path.join(__dirname, "assets")));
 app.use(cookie(process.env.SECRET));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.set("view engine", "ejs");
+app.set("views", path.join(__dirname + `/views`));
 app.use(router);
 app.get("/", (req, res) => res.send("running"));
 
